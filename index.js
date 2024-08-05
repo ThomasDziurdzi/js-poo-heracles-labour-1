c0b1275073652c1d364eaf317aa33e869f3c3dbd
Thomas Dziurdzi <63227823+ThomasDziurdzi@users.noreply.github.com>
2024-08-05 22:00:10 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+const Fighter = require("./src/Fighter");
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const heracles = new Fighter("🧔‍♂️ Heracles", 20, 6);
+const nemean = new Fighter("🦁 Nemean Lion", 11, 13);
+
+let round = 1;
+while (heracles.isAlive()  && nemean.isAlive()) {
+    console.log(`Round ${round}: `);
+    heracles.fight(nemean);
+    console.log(`${heracles.name} attacks ${nemean.life}!`);
+    console.log(`${nemean.name} has ${nemean.life} life points left.`);
+
+    if (nemean.isAlive()) {
+        nemean.fight(heracles);
+        console.log(`${nemean.name} attacks ${heracles.life}!`);
+        console.log(`${heracles.name} has ${heracles.life} life points left.`);
+    }
+
+    round++;
+
+    if (!heracles.isAlive()) {
+        console.log(`${nemean.name} wins!`);
+        console.log(`${heracles.name} is defeated.`);
+    } else if (!nemean.isAlive()) {
+        console.log(`${heracles.name} wins!`);
+        console.log(`${nemean.name} is defeated.`);
+    }
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,23 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor(name, strength, dexterity, life) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    }
+
+    fight(defender) {
+    const attackDamage = Math.floor(Math.random() * this.strength) + 1;
+    const defenseValue = Math.max(defender.dexterity - attackDamage, 0);
+    const damage = attackDamage - defenseValue;
+    defender.life = Math.max(defender.life - damage, 0);
+    }
+
+    isAlive() {
+        return this.life > 0;
+      }
+}
+module.exports = Fighter;
